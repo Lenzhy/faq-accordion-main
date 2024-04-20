c8218cc28594540c41a664e6ce7355a85003bf07
Lenzhy <lenny.sadonpignon@gmail.com>
2024-04-21 00:46:56 +0200

    Add navigation with arrow JS

--- hide.js
+++ hide.js
@@ -41,4 +41,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     buttonsMoins.forEach((button, index) => {
         button.addEventListener("click", () => handleMoinsButtonClick(index));
     });
+
+
+    let keyIndex = 0;                                 // function when press arrows on keyboard it make text appear in order
+    document.addEventListener("keydown",(keyDown));      //addEvent press key
+    function keyDown(e){
+        if (e.key==="ArrowDown" && keyIndex<5){               // incrementation
+            keyIndex = keyIndex + 1;
+        }
+        else if (e.key==="ArrowDown" && keyIndex>=5){
+            keyIndex= 1;
+        }
+        else if (e.key==="ArrowUp" && keyIndex>=1){
+            keyIndex = keyIndex - 1;
+        }
+        else if (e.key==="ArrowUp" && keyIndex<1){
+            keyIndex = 4;
+        }
+        console.log(e.key)
+        handlePlusButtonClick(keyIndex-1);          //-1 because the index of an array start at 0
+
+    }
 });
